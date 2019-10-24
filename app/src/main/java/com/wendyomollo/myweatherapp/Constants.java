8d81b436c14cc9827de44d5d530a81dae8ad355d
WendyOmollo <omollow280@gmail.com>
2019-10-24 10:26:43 +0300

    Add an API key

---
+++ app/src/main/java/com/wendyomollo/myweatherapp/Constants.java
@@ -0,0 +1,6 @@
+package com.wendyomollo.myweatherapp;
+
+public class Constants {
+    public static final String BASE_URL = "http://api.openweathermap.org/data/2.5/forecast?";
+    public static final String WEATHER_API_KEY = BuildConfig.APPID;
+}
