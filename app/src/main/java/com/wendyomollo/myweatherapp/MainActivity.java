a4048138a29b8cacd95041744f1696cd4a99c651
WendyOmollo <omollow280@gmail.com>
2019-10-25 09:38:08 +0300

    Change layout

--- app/src/main/java/com/wendyomollo/myweatherapp/MainActivity.java
+++ app/src/main/java/com/wendyomollo/myweatherapp/MainActivity.java
@@ -4,11 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import static com.wendyomollo.myweatherapp.BuildConfig.APPID;
+import static com.wendyomollo.myweatherapp.Constants.BASE_URL;
+import static com.wendyomollo.myweatherapp.Constants.WEATHER_API_KEY;
+
 public class MainActivity extends AppCompatActivity {
+
+    public static String BaseUrl = BASE_URL;
+    public static String AppId = WEATHER_API_KEY;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
