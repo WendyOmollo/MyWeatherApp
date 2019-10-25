9aa93c5f9220c4fa53a18d9678db2a3fba07ffeb
WendyOmollo <omollow280@gmail.com>
2019-10-25 10:50:54 +0300

    Add a method that retrieves info about the weather of a city

--- app/src/main/java/com/wendyomollo/myweatherapp/MainActivity.java
+++ app/src/main/java/com/wendyomollo/myweatherapp/MainActivity.java
@@ -3,6 +3,8 @@ package com.wendyomollo.myweatherapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import static com.wendyomollo.myweatherapp.BuildConfig.APPID;
 import static com.wendyomollo.myweatherapp.Constants.BASE_URL;
@@ -12,11 +14,20 @@ public class MainActivity extends AppCompatActivity {
 
     public static String BaseUrl = BASE_URL;
     public static String AppId = WEATHER_API_KEY;
+    private Button mSearchCity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mSearchCity = findViewById(R.id.buttonSearch);
+        mSearchCity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                generateCity();
+            }
+        });
 
 
     }
